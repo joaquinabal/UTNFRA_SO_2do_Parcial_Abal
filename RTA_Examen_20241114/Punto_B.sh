e1323924bcf7419901a72f4ef9b6d2370b1de505
Joaquin Abal <joabal97@gmail.com>
2024-11-15 23:00:18 +0000

    Nueva VM y nuevo Usuario

--- RTA_Examen_20241114/Punto_B.sh
+++ RTA_Examen_20241114/Punto_B.sh
@@ -1,4 +1,3 @@
-#! /bin/bash
+ #! /bin/bash
 
-echo "Ejecutar sudo bash /usr/local/bin/AbalUser-Groups.sh vagrant  ~/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt"
-
+echo "Ejecutar sudo bash /usr/local/bin/joaquinabal/AbalUser-Groups.sh joaquinabal /home/joaquinabal/UTNFRA_SO_2do_Parcial_Abal/UTN-FRA-SO_Examenes/202406/bash_script/Lista_Usuarios.txt"
