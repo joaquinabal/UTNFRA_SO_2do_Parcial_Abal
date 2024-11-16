09a52b26dc52ed1212080b590d0b24a7758e0f24
Joaquin Abal <joabal97@gmail.com>
2024-11-16 20:49:07 +0000

    Punto D

---
+++ RTA_Examen_20241114/Punto_D.sh
@@ -0,0 +1,4 @@
+!# /bin/bash
+
+sudo ansible-playbook -i /home/joaquinabal/UTNFRA_SO_2do_Parcial_Abal/UTN-FRA-SO_Examenes/202406/ansible/inventory /home/joaquinabal/UTNFRA_SO_2do_Parcial_Abal/UTN-FRA-SO_Examenes/202406/ansible/playbook.yml
+
